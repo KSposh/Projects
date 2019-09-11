553a4116fe3b4a06efe2abac40b50b814d2bef54
KSposh <jose.marques@elecnor.es>
2019-09-11 17:05:46 +0100

    teet

--- python_mega/app_6/app_6.py
+++ python_mega/app_6/app_6.py
@@ -3,19 +3,20 @@ import time
 
 video = cv2.VideoCapture(0)
 
-a = 0
+
 while True:
     a = a + 1
     check, frame = video.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if
+
     cv2.imshow("Capturing", gray)
-    cv2.waitKey(1000)
+    cv2.waitKey(1)
 
     if key == ord('q'):
         break
-
-print(f"Ran for {a} seconds!")
 
 video.release()
 
